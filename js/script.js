5b319bbd384a8e53f149cc368d85443b73c3b0eb
Sabin Subedi <sabinsubedi125@gmail.com>
2020-09-07 09:53:57 +0545

    I have completed making the prtfolio site

--- js/script.js
+++ js/script.js
@@ -11,3 +11,40 @@ for (let i = 0; i < card.length; i++) {
     card_body[i].style.visibility = "hidden";
   });
 }
+
+$(window).scroll(function () {
+  if ($(this).scrollTop() > 150) {
+    $(".back-to-top").addClass("visible");
+  } else {
+    $(".back-to-top").removeClass("visible");
+  }
+});
+var x = 0;
+$("#ham").on("click", function () {
+  x += 1;
+  $(".nav-full-item-links").toggleClass("visible");
+  if (x % 2 != 0) {
+    $(".nav-full-item").addClass("animation");
+    $(".nav-full-item").removeClass("down");
+  } else {
+    $(".nav-full-item").removeClass("animation");
+    $(".nav-full-item").addClass("down");
+  }
+});
+
+$(".nav-full-item-links a").on("click", function () {
+  $(".nav-full-item-links").removeClass("visible");
+  $("#ham").toggleClass("active");
+});
+
+$("#work-tab,#works-tab,#about-tab,#abouts-tab").on("click", function () {
+  $("body").removeClass("bg-red");
+  $(".nav-full-item-links").removeClass("bg-red");
+  $(".nav-social-media a,.nav-link,.navbrand").removeClass("color-white");
+});
+
+$("#contact-tab,#contacts-tab").on("click", function () {
+  $("body").addClass("bg-red");
+  $(".nav-full-item-links").addClass("bg-red");
+  $(".nav-social-media a,.nav-link,.navbrand").addClass("color-white");
+});
